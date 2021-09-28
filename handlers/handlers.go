749db970552bc92ad97fec8e803afb0d5e555311
Daniel Eichhorn <daniel@shipt.com>
2021-09-27 22:50:55 -0500

    updated post request function

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,35 +5,27 @@ import (
 	"MovieDatabase/repo"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
-
 func PostNewMovie(w http.ResponseWriter, r *http.Request) {
 	mv := entities.Movie{}
-
 	err := json.NewDecoder(r.Body).Decode(&mv)
-	if err != nil{
-		fmt.Println(err)
-	}
-
-	mv.GetId()
-
-	mvdb := repo.DataBase{}
-	mvdb.AddToDb(mv)
-
-	movieBytes, err := json.MarshalIndent(mvdb, "", "	")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	err = ioutil.WriteFile("moviedb.json", movieBytes, 0644)
-	if err != nil{
+	db, err := repo.AddMovie(mv)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	movieBytes, err := json.MarshalIndent(db, "", "	")
+	if err != nil {
 		fmt.Println(err)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(movieBytes)
+	_, err = w.Write(movieBytes)
 }
--- repo/repo.go
+++ repo/repo.go
@@ -2,13 +2,37 @@ package repo
 
 import (
 	"MovieDatabase/entities"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 )
 
-type DataBase struct{
+type DataBase struct {
 	Movies []entities.Movie `json:"Movies"`
 }
 
-func (db *DataBase) AddToDb(movie entities.Movie) {
-	db.Movies = append(db.Movies, movie)
-}
+func AddMovie(m entities.Movie) (DataBase, error) {
+	file, err := ioutil.ReadFile("moviedb.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	m.GetId()
+	dbSlice := DataBase{}
+	err = json.Unmarshal(file, &dbSlice)
+	if err != nil {
+		fmt.Println(err)
+	}
+	dbSlice.Movies = append(dbSlice.Movies, m)
 
+	movieBytes, err := json.MarshalIndent(dbSlice, "", "	")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = ioutil.WriteFile("moviedb.json", movieBytes, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return dbSlice, nil
+}
